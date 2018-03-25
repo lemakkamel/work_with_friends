28993790b525228dd2cb727f88a915864d6b1f59
kamel_1 <derbali1987@gmail.com>
2018-03-25 17:51:24 +0200

    delete two function first_letter and second_letter

--- draw_letters.py
+++ draw_letters.py
@@ -1,27 +1,29 @@
 from __future__ import print_function
-def first_letter(i):
-    if i ==1 or i==5:
-        print ("*"+" "+"*",end="")
-    if i ==2 or i==4:
-        print ("*"*2+" ",end="")
-    if i ==3:
-        print ("*"+" "*2,end="")
-
-def second_letter(i):
-    if i ==1 :
-        print (" "+"*"*2+" ",end="")
-    if i ==2 or i==4 or i==5:
-        print ("*"+" "*2+"*",end="")
-    if i ==3:
-        print ("*"*4,end="")
+def make_draw(letter, i):
+    print(" " * 4, end="")
+    if (letter=="a" and i ==1):
+        print(" " + "*" * 2 + " ", end="")
+    if (letter=="a" and i ==2)or(letter=="a" and i ==4)or(letter=="a" and i ==5) :
+        print("*" + " " * 2 + "*", end="")
+    if (letter=="a" and i ==3):
+        print("*" * 4, end="")
+    if ((letter=="k" and i ==3)or (letter=="l"and (i==1 or i==2 or i==3 or i==4))):
+        print("*" + " " * 2, end="")
+    if (letter == "k" and (i == 1 or i == 5)or(letter=="l"and i==5 )):
+        print("*" + " " + "*", end="")
+    if (letter == "k" and i == 2) or (letter == "k" and i == 4):
+        print("*" * 2 + " ", end="")
 
 def draw_letter(string):
     print ("here will draw ",string)
+    print()
+    x=len(string)
+
     for i in range(1,6):
-        first_letter(i)
-        print(" "*4,end="")
-        second_letter(i)
+        for j in range(0,x):
+            #print (j)
+            make_draw(string[j],i)
         print ()
 
-
-draw_letter("KA")
+string_to_draw=raw_input("Enter you string : ")
+draw_letter(string_to_draw)
