c990bb0ce81c94789492ab36c77b4a4d210db809
kamel_1 <derbali1987@gmail.com>
2018-03-28 15:25:36 +0100

    Create atm solution Folder and atm.py file in it

---
+++ atm solution/atm.py
@@ -0,0 +1,21 @@
+def aTM(balance,request):
+	if request>balance or request<0:
+		print "you can't"
+	else:
+		while request > 0:
+			if request >= 100:
+				request -= 100
+				print "give ", 100
+			if request >= 50 and request < 100:
+				request -= 50
+				print "give ", 50
+			if request >= 10 and request < 50:
+				request -= 10
+				print "give ", 10
+			if request >= 5 and request < 10:
+				request -= 5
+				print "give ", 5
+			if request < 5 and request != 0:
+				print "give ", request
+				request = 0
+aTM(500,277)
