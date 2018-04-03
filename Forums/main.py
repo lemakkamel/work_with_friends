602ed526867d52316bb6f6c6204fdd2090261380
kamel_1 <derbali1987@gmail.com>
2018-04-03 18:09:56 +0100

    Add '__str__' function to 'Member & Post' classes

--- Forums/main.py
+++ Forums/main.py
@@ -3,31 +3,28 @@ import stores
 
 member1=models.Member("Bahaa",5)
 member2=models.Member("wassim",10)
+member3=models.Member("kamel",20)
 
 post1=models.Post("Python","This is an introduction to python")
 post2=models.Post("Variables","A name that refers to a value.")
 post3=models.Post("Assignment","A statement that assigns a value to a variable.")
 print "="*35
-print "This is Name of member One ",member1.name
-print "This is Age of member One ",member1.age
+print member1
 print "="*35
-print "This is Name of member Two ",member2.name
-print "This is Age of member Two ",member2.age
+print member2
 print "="*35
 print "="*7+"This is first post "+"="*7
-print post1.post_title
-print post1.post_content
+print post1
 print "="*7+"This is second post "+"="*7
-print post2.post_title
-print post2.post_content
+print post2
 print "="*7+"This is third post "+"="*7
-print post3.post_title
-print post3.post_content
+print post3
 
 
 member_store = stores.MemberStore()
 member_store.add(member1.name)
 member_store.add(member2.name)
+member_store.add(member3.name)
 print "+"*15+"This will print all members !!"+"+"*15
 print member_store.get_all()
 print "+"*15+"This will print all posts !!"+"+"*15
--- Forums/models.py
+++ Forums/models.py
@@ -2,13 +2,17 @@ class Member:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
+    def __str__(self):
+        return "Member name: {}, member age: {}".format(self.name,self.age)
 
 
 class Post:
     def __init__(self, post_title, post_content):
         self.post_title = post_title
         self.post_content = post_content
+
+    def __str__(self):
+        return "The title : {}\nContent: {}".format(self.post_title,self.post_content)
 
 
 
